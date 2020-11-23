a0855812d05fa46a9c8c052b3a5f00f85a3d1525
Max Heidinger <mail@max-heidinger.de>
2020-11-23 10:09:11 +0100

    Initial Commit

---
+++ src/main/java/com/digitaldoctor/digitaldoctor/DigitaldoctorApplication.java
@@ -0,0 +1,13 @@
+package com.digitaldoctor.digitaldoctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitaldoctorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitaldoctorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitaldoctor/digitaldoctor/DigitaldoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitaldoctor.digitaldoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitaldoctorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
